aaccefb0dd6a997622628e1fc57b51192cf762cb
Krishna kumar <whoiswho.why@outlook.com>
2025-08-15 19:44:32 +0530

    app completed locally

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # medical-chatbot
+following steps are done to make the project
+
+1. initilized the github
+2. make the folder sturcture
+3. create virtual environment
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Krishna rai",
+    author_email="482krishnarai@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
